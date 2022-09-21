8acf1c66973d35c889eb90fb1fc52cc5b5483745
Car00l <anacarolinemartineli02@gmail.com>
2022-09-21 06:03:36 -0300

    salvar

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function(e) {
 
     $.ajax({
         type: 'GET',
-        dataType: 'JSON',
         url: `https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome`,
+        dataType: 'JSON',
         success: function(dados) {
-            $('#UF').val(dados.sigla)
+            $.each(dados, function (indexInArray, valueOfElement){
+                var opicao = "<option>" + valueOfElement.sigla + "</option>";
+                $('#UF').append(opicao);
+            })
         }
     })
 
+    var uf = $('#UF').val();
+    $.ajax({
+        type: 'GET',
+        url: `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios?orderBy=nome`,
+        dataType: 'JSON',
+        success: function(dados) {
+            $.each(dados, function (indexInArray, valueOfElement){
+        var opicao = "<option>" + valueOfElement.nome + "</option>";
+        $('#CIDADE').append(opicao);
+    })
+    }
+})
 
 })
